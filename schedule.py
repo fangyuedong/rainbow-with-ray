3e4fb743fe40467a5e6c8d9b18418ba9fc29e65d
fangyuedong <fangyuedong@gmail.com>
2020-03-16 00:42:43 +0800

    add remote function support

--- schedule.py
+++ schedule.py
@@ -7,12 +7,21 @@ class Sched():
     def __init__(self):
         self.tsk_info = {}
 
-    def add(self, actor, method="", **kwargs):
-        assert isinstance(actor, ray.actor.ActorHandle)
-        tsk_id = getattr(actor, method).remote(**kwargs)
-        self.tsk_info[tsk_id] = RayTsk(actor, method, kwargs, 
-            actor._ray_actor_creation_function_descriptor.class_name)
-        print("start\t{}.{}({})".format(actor, method, kwargs))
+    def add(self, actor=None, method=None, **kwargs):
+        # exec some class method
+        if isinstance(actor, ray.actor.ActorHandle) and hasattr(actor, method):
+            tsk_id = getattr(actor, method).remote(**kwargs)
+            self.tsk_info[tsk_id] = RayTsk(actor, method, None, 
+                actor._ray_actor_creation_function_descriptor.class_name)
+            print("start\t{}.{}({})".format(actor, method, None))
+        # exec some func
+        elif callable(method) and actor is None:
+            tsk_id = ray.remote(method).remote(**kwargs)
+            self.tsk_info[tsk_id] = RayTsk(actor, method.__name__, None, None)
+            print("start\t{}.{}({})".format(actor, method.__name__, None))
+        else:
+            raise NotImplementedError
+        return tsk_id
 
     def wait(self, num_tsk=1):
         """wait until num of tasks are done"""
@@ -20,6 +29,3 @@ class Sched():
             "doing tasks num {} is less than require num {}".format(len(self.tsk_info), num_tsk)
         done_tsks, _ = ray.wait(list(self.tsk_info.keys()), num_tsk)
         return done_tsks, [self.tsk_info.pop(tsk_id) for tsk_id in done_tsks]
-
-    
-
