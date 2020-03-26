04a47a5a845c7433dc968a0fed8d335ecc595c94
fangyuedong <fangyuedong@gmail.com>
2020-03-26 22:23:38 +0800

    code clean

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -5,36 +5,31 @@ import ray
 import random
 import numpy as np
 sys.path.append("./")
-from utils.replay_buffer import mmdb_op as lmdb_op
+from utils.replay_buffer import mmdb_op as db_op
 from utils.dataloader import Dataloader
 from agent import BasicWorker
 import torch
-import gc
 
 class TestCase(unittest.TestCase):
 
     def test_1_worker(self):
         buffer = "./ut_lmdb"
-        buffer = lmdb_op.init(buffer)
-        exc_worker = BasicWorker(db=buffer, db_write=lmdb_op.write)
-        dataloader = Dataloader(buffer, lmdb_op, batch_size=64, worker_num=4, batch_num=40)
+        buffer = db_op.init(buffer)
+        exc_worker = BasicWorker(db=buffer, db_write=db_op.write)
+        dataloader = Dataloader(buffer, db_op, batch_size=256, worker_num=4, batch_num=10)
         for _ in range(20):
             _ = exc_worker.__next__()
-            print(lmdb_op.len(buffer))
+            print(db_op.len(buffer))
         count = 0
         t0 = time.time()
         for data in dataloader:
             fd = {k: torch.from_numpy(v) for k, v in data.items()}
             fd = {k: v.cuda().float() for k, v in fd.items()}
-            # data.clear()
-            # fd.clear()
-            # del fd, data
-            # gc.collect()
             count += 1
-            # if count == 4000:
-            #     break
+            if count == 1000:
+                break
         print(time.time() - t0)
-        lmdb_op.clean(buffer)
+        db_op.clean(buffer)
             
 
     
