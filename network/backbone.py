81ad6851651140f2abe55a6314b6067857c82ee5
fangyuedong <fangyuedong@gmail.com>
2020-05-31 21:29:19 +0800

    using batch norm in network

--- network/backbone.py
+++ network/backbone.py
@@ -4,23 +4,36 @@ import torch.nn.functional as F
 import numpy as np
 
 class BasicNet(nn.Module):
-    def __init__(self, shape, na, norm=True):
+    def __init__(self, shape, na, norm="batch_norm"):
         """
         shape: observation shape
         na:    action number
         """
         super(BasicNet, self).__init__()
+        assert norm in ["batch_norm", "group_norm", None]
         self.shape = shape
         self.na = na
-        if norm:
+        if norm == "batch_norm":
             self.convs = nn.Sequential(
-                nn.Conv2d(shape[0], 32, 8, 4),
+                nn.Conv2d(shape[0], 32, 8, 4, bias=False),
+                nn.BatchNorm2d(32),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(32, 64, 4, 2, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 64, 3, 1, bias=False),
+                nn.BatchNorm2d(64),
+                nn.ReLU(inplace=True)
+            )            
+        elif norm == "group_norm":
+            self.convs = nn.Sequential(
+                nn.Conv2d(shape[0], 32, 8, 4, bias=False),
                 nn.GroupNorm(16, 32),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(32, 64, 4, 2),
+                nn.Conv2d(32, 64, 4, 2, bias=False),
                 nn.GroupNorm(16, 64),
                 nn.ReLU(inplace=True),
-                nn.Conv2d(64, 64, 3, 1),
+                nn.Conv2d(64, 64, 3, 1, bias=False),
                 nn.GroupNorm(16, 64),
                 nn.ReLU(inplace=True)
             )
@@ -33,6 +46,7 @@ class BasicNet(nn.Module):
                 nn.Conv2d(64, 64, 3, 1),
                 nn.ReLU(inplace=True)
             )
+        self.norm = norm
 
     
     def forward(self, x):
