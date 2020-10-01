37eaaa3e4bc0c980f79245f54aed112ed02c9620
fangyuedong <fangyuedong@gmail.com>
2020-10-02 02:00:34 +0800

    pin memory first commit

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -5,7 +5,7 @@ import ray
 import random
 import numpy as np
 sys.path.append("./")
-from utils.replay_buffer import pmdb_op as db_op
+from utils.replay_buffer import mmdb_op as db_op
 from utils.dataloader import Dataloader
 from agent import BasicWorker
 import torch
@@ -16,20 +16,21 @@ class TestCase(unittest.TestCase):
         buffer = "./ut_lmdb"
         buffer = db_op.init(buffer)
         exc_worker = BasicWorker(db=buffer, db_write=db_op.write)
-        dataloader = Dataloader(buffer, db_op, batch_size=256, worker_num=4, batch_num=10)
         for _ in range(20):
             _ = exc_worker.__next__()
             print(db_op.len(buffer))
+        dataloader = Dataloader(buffer, db_op, batch_size=256, worker_num=4, batch_num=20)
         count = 0
         t0 = time.time()
-        for data, _, _ in dataloader:
-            fd = {k: torch.from_numpy(v) for k, v in data.items()}
-            fd = {k: v.cuda().float() for k, v in fd.items()}
+        for data, _, _, p in dataloader:
+            # fd = {k: torch.from_numpy(v) for k, v in data.items()}
+            # fd = {k: v.cuda().float() for k, v in fd.items()}
             count += 1
             if count == 1000:
                 break
         print(time.time() - t0)
         db_op.clean(buffer)
+        del dataloader
             
 
     
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,11 +1,15 @@
 import sys,os
-import numpy as np
 import torch
-import ray
+import threading, queue
 import time
+import ray
+import numpy as np
+from torch.utils.data._utils.pin_memory import pin_memory
+from torch._utils import ExceptionWrapper
 sys.path.append("./")
 from utils.replay_buffer import DataOp, lmdb_op
 
+MP_STATUS_CHECK_INTERVAL = 5.0
 """
 Transition = {"state": np.array, "action": int, "next_state": np.array, "reward": float, "done": logical}
 """
@@ -27,6 +31,12 @@ def batch4net(batch, cuda=True):
     f = f_cuda if cuda else f_cpu
     batch = {k: f(k, v) for k, v in batch.items()}
     return batch
+
+def tnx2tensor(tnx):
+    return {k: torch.from_numpy(v) for k, v in tnx.items()}
+
+def prior2tensor(prior):
+    return  torch.from_numpy(prior)
 
 @ray.remote
 def pre_fetch_worker(work_func, dataset, num, batch_size):
@@ -58,24 +68,81 @@ class Dataloader():
         self.tsks = []
         self.cache = []
         self.update_prior_tsks = []
+        self.thread_event = threading.Event()
+        self.prefetch_queue = queue.Queue(maxsize=batch_num+1)
+        self.pin_queue = queue.Queue(maxsize=batch_num+1)
+        self.prefetch_thread = threading.Thread(
+            target=self.pre_fetch, 
+            args=(self.prefetch_queue, self.thread_event))
+        self.pin_thread = threading.Thread(
+            target=self.pin_memory, 
+            args=(self.prefetch_queue, self.pin_queue, torch.cuda.current_device(), self.thread_event))
+        self.prefetch_thread.start()
+        self.pin_thread.start()
+
+    def pre_fetch(self, out_queue, done_event):
+        while len(self.tsks) < self.worker_num:
+            self.tsks.append(pre_fetch_worker.remote(self.data_op.sample, self.dataset, 1, self.batch_size))
+        tsk_dones = []
+        while not done_event.is_set():
+            try:
+                if len(tsk_dones) == 1:
+                    r = ray.get(tsk_dones[0])[0]
+                    tnx, item_id, item_idx, prior = r
+                    r = (tnx2tensor(tnx), item_id, item_idx, prior2tensor(prior))
+                    out_queue.put(r, timeout=MP_STATUS_CHECK_INTERVAL)
+                    self.tsks.append(pre_fetch_worker.remote(self.data_op.sample, self.dataset, 1, self.batch_size))
+            except queue.Full:
+                time.sleep(0.001)
+                continue
+            tsk_dones, self.tsks = ray.wait(self.tsks)
+    
+    def pin_memory(self, in_queue, out_queue, device_id, done_event):
+        torch.set_num_threads(1)
+        torch.cuda.set_device(device_id)
+        while not done_event.is_set():
+            try:
+                r = in_queue.get(timeout=MP_STATUS_CHECK_INTERVAL)
+            except queue.Empty:
+                continue
+            tnx, item_id, item_idx, prior = r
+            data = tnx, prior
+            if not done_event.is_set() and not isinstance(data, ExceptionWrapper):
+                try:
+                    data = pin_memory(data)
+                except Exception:
+                    data = ExceptionWrapper(
+                        where="in pin memory thread for device {}".format(device_id))
+                tnx, prior = data
+                r = tnx, item_id, item_idx, prior
+            while not done_event.is_set():
+                try:
+                    out_queue.put(r, timeout=MP_STATUS_CHECK_INTERVAL)
+                    break
+                except queue.Full:
+                    continue
+            del r  # save memory
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        while len(self.tsks) < self.worker_num:
-            self.tsks.append(pre_fetch_worker.remote(self.data_op.sample, self.dataset, self.batch_num, self.batch_size))
-
-        if len(self.cache) == 0:
-            tsk_dones, self.tsks = ray.wait(self.tsks)
-            assert len(tsk_dones) == 1
-            self.cache = ray.get(tsk_dones[0])
-            return self.cache.pop(0)
-        else:
-            return self.cache.pop(0)  
+        while 1:
+            try:
+                data = self.pin_queue.get(timeout=MP_STATUS_CHECK_INTERVAL)
+                break
+            except queue.Empty:
+                continue
+        return data
 
     def update(self, idxs, priors, check_ids):
         if len(self.update_prior_tsks):
             _, self.update_prior_tsks = ray.wait(self.update_prior_tsks)
         self.update_prior_tsks.append(update_prior_worker.remote(self.data_op.update, self.dataset, idxs, priors, check_ids))
+
+    def __del__(self):
+        print("delete")
+        self.thread_event.set()
+        self.prefetch_thread.join()
+        self.pin_thread.join()
 
