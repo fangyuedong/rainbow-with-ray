9ce1df235ce06451bea66d6cf282bfbeff50a50d
fangyuedong <fangyuedong@gmail.com>
2020-10-14 19:33:20 +0800

    dataloader test

--- test/test_dataloader.py
+++ test/test_dataloader.py
@@ -5,9 +5,9 @@ import ray
 import random
 import numpy as np
 sys.path.append("./")
-from utils.replay_buffer import pmdb_op as db_op
+from utils.replay_buffer import mmdb_op as db_op
 from utils.dataloader import Dataloader
-from agent import BasicWorker
+from agent import BasicWorker, DQN_Worker
 import torch
 
 class TestCase(unittest.TestCase):
@@ -15,16 +15,22 @@ class TestCase(unittest.TestCase):
     def test_1_worker(self):
         buffer = "./ut_lmdb"
         buffer = db_op.init(buffer)
-        exc_worker = BasicWorker(db=buffer, db_write=db_op.write)
-        dataloader = Dataloader(buffer, db_op, batch_size=256, worker_num=4, batch_num=10)
-        for _ in range(20):
+        # exc_worker = BasicWorker(db=buffer, db_write=db_op.write)
+        exc_worker = DQN_Worker(db=buffer, db_write=db_op.write)
+        # exc_worker.update(eps=0.9)
+        dataloader = Dataloader(buffer, db_op, batch_size=256, worker_num=8, batch_num=20)
+        # for _ in range(20):
+        t0 = time.time()
+        while db_op.len(buffer) < 10000:
             _ = exc_worker.__next__()
-            print(db_op.len(buffer))
+            # print(db_op.len(buffer))
+        print(time.time() - t0)
         count = 0
         t0 = time.time()
-        for data, _, _ in dataloader:
+        for data, _, _, _ in dataloader:
             fd = {k: torch.from_numpy(v) for k, v in data.items()}
             fd = {k: v.cuda().float() for k, v in fd.items()}
+            time.sleep(0.02)
             count += 1
             if count == 1000:
                 break
